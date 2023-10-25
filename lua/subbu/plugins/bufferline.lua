521db9ba8626e0c8a261e67263b516dcfc3d4666
subbu.alagappan <subbu.alagappan@eqtpartners.com>
2023-10-25 23:05:18 +0200

    feat: Added syntax highlighting for go and rust

--- lua/subbu/plugins/bufferline.lua
+++ lua/subbu/plugins/bufferline.lua
@@ -1,19 +1,19 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "buffer",
-      separator_style = "slant",
-      offsets = {
-        {
-          filetype = "NvimTree",
-          text = "File Explorer", 
-          text_align = "center", 
-          separator = true,
-        }
-      },
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "buffer",
+			separator_style = "slant",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					text_align = "center",
+					separator = true,
+				},
+			},
+		},
+	},
 }
