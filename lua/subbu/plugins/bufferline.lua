7afb2b0f4a539b1d942d121bb564c226b901af1e
subbu.alagappan <subbu.alagappan@eqtpartners.com>
2023-10-28 21:56:31 +0200

    feat: Added bufferline keymaps

--- lua/subbu/plugins/bufferline.lua
+++ lua/subbu/plugins/bufferline.lua
@@ -2,18 +2,25 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "buffer",
-			separator_style = "slant",
-			offsets = {
-				{
-					filetype = "NvimTree",
-					text = "File Explorer",
-					text_align = "center",
-					separator = true,
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffer",
+				separator_style = "slant",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+					},
 				},
 			},
-		},
-	},
+		})
+
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "<leader>bl", "<cmd>BufferLinePick<CR>", { desc = "Pick from open buffers to go to" })
+		keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { desc = "Move to next buffer" })
+		keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", { desc = "Move to prev buffer" })
+	end,
 }
